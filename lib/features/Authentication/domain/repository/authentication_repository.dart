759feed8f5d923e0c912560ac370c840d02b0a73
Emeka-Chukwu <emekapascal11@gmail.com>
2021-01-21 10:15:11 +0100

    added abstract class of authentication

--- lib/features/Authentication/domain/repository/authentication_repository.dart
+++ lib/features/Authentication/domain/repository/authentication_repository.dart
@@ -5,4 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthenticationRepository {
   Future<Either<Failure, UserCredential>> registerUser(UserEntity userEntity);
+  Future<Either<Failure, UserCredential>> loginUser(UserEntity userEntity);
+  Future<Either<Failure, UserCredential>> saveTODatabase(UserEntity userEntity);
+  Future<Either<Failure, bool>> logout();
 }
