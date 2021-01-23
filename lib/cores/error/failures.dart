6b82fc124e42bbdaf3c359ca7919c04f58af8ece
Emeka-Chukwu <emekapascal11@gmail.com>
2021-01-23 16:08:03 +0100

    added failure error

--- lib/cores/error/failures.dart
+++ lib/cores/error/failures.dart
@@ -4,3 +4,12 @@ abstract class Failure extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class AuthenticationFailure extends Failure {
+  final String message;
+
+  AuthenticationFailure({this.message});
+
+  @override
+  List<Object> get props => [message];
+}
