33ea6ab968e8b3cf094870b96b43ce2fdf870130
Emeka-Chukwu <emekapascal11@gmail.com>
2021-01-23 16:09:42 +0100

    modified

--- lib/features/Authentication/domain/repository/authentication_repository.dart
+++ lib/features/Authentication/domain/repository/authentication_repository.dart
@@ -1,11 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:envoy/cores/error/failures.dart';
+import 'package:envoy/features/Authentication/data/model/user_model.dart';
 import 'package:envoy/features/Authentication/domain/entity/user_entity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthenticationRepository {
-  Future<Either<Failure, UserCredential>> registerUser(UserEntity userEntity);
-  Future<Either<Failure, UserCredential>> loginUser(UserEntity userEntity);
-  Future<Either<Failure, UserCredential>> saveTODatabase(UserEntity userEntity);
-  Future<Either<Failure, bool>> logout();
+  Future<Either<AuthenticationFailure, UserCredential>> registerUser(
+      UserEntity userEntity);
+  Future<Either<AuthenticationFailure, UserCredential>> loginUser(
+      UserEntity userEntity);
+  Future<Either<AuthenticationFailure, bool>> saveTODatabase(
+      UserEntity userEntity);
+  Future<Either<AuthenticationFailure, bool>> logout();
 }
